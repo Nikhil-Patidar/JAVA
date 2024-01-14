fae40c7235e0e645c99239800df41e750447a253
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-14 22:28:32 +0530

    Add files via upload

---
+++ Areaoftringle.java
@@ -0,0 +1,15 @@
+class Area{
+  int h;
+int b;
+
+
+public static void main(String a[]){
+    float area;
+    Area A=new Area();
+   A.h=10;
+A.b=20;
+
+area=(A.h*A.b)/2;
+System.out.println(area);
+ }
+}
