5732892bf580dd38dbfce7cdddeb2bd0f3980159
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-15 20:32:12 +0530

    Add files via upload

---
+++ ArRect.java
@@ -0,0 +1,11 @@
+class ArRect{
+ int l=10;
+ int b=20;
+public static void main(String a[]){
+ 
+ int area;
+ArRect A=new ArRect();
+area=A.l*A.b;
+System.out.println("Area of Rectangle : "+area);
+}
+}
