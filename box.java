eb0b9ba858ae1ea3b223261438cfe75ac59667f4
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-15 20:32:33 +0530

    Add files via upload

---
+++ box.java
@@ -0,0 +1,13 @@
+class Box{
+            int p=40;
+            int q=50;
+           int r=60;
+  public static void main(String a[]){
+    int x=10;
+    int y=20;
+    int z=30;
+Box b=new Box();
+System.out.println("without objects the bolme of box  : "+x*y*z);
+System.out.println("with objects the bolme of box : "+b.p*b.q*b.r); 
+}
+}
