a6bc0e49db7fbe3236eacd9afa314b49f5a77a38
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-15 20:32:49 +0530

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,23 @@
+public import java.util.Scanner;
+class Display{
+ public static void main(String a[]){
+int x;
+float y;
+double z;
+String s;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of int type");
+x=sc.nextInt();
+System.out.println("Enter the value of float type");
+y=sc.nextFloat();
+System.out.println("Enter the value of double type");
+z=sc.nextDouble();
+System.out.println("Enter the value of string type");
+s=sc.next();
+System.out.println("Show the all value \n"+x);
+System.out.println(y);
+System.out.println(z);
+System.out.println(s);
+}
+}
