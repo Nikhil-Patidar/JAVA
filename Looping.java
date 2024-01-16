6512b16912deaeaa4360474be7d6d22fe28570f8
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-17 01:11:07 +0530

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Looping
+{
+public static void main(String a[])
+{
+ int n,i,sum=0;
+Scanner Sc=new Scanner(System.in);
+    n=Sc.nextInt();
+for(i=1;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+System.out.println("The sum of "+n+" numbers : "+sum);
+}
+}
