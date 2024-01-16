96b9feed21ffe316eaf98bae89422223096e228c
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-16 15:17:54 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Even
+{
+public static void main(String a[])
+{
+ int n;
+Scanner Sc=new Scanner(System.in);
+n=Sc.nextInt();
+if(n%2==0)
+{
+ System.out.println("the number is even");
+
+}
+else
+System.out.println("The number is odd");
+}
+}
