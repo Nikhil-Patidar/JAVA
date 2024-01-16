b72c620516d1dfe1998a3972200ba34a6be76da6
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-17 01:10:47 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,10 @@
+class ExampletypeCasing
+{
+    public Static void main(Sting a[])
+    {
+        byte b;
+        int x=10;
+        b=(byte)x;
+    System.out.print("implict casting "+i)
+    }
+}
