bbabab6ef14a10f3f54940e9cb830d9413835ee4
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-19 17:57:19 +0530

    Add files via upload

---
+++ Read.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Read
+{ 
+ public static void main(String a[])
+{
+ 
+  int id,salary;
+  String name;
+ Scanner ab=new Scanner(System.in);
+  name=ab.next();
+  id=ab.nextInt();
+  salary=ab.nextInt();
+ System.out.println("Name is : "+name+"\nEmployee id is : "+id+"\nEmployee salary is : "+salary);
+}
+}
